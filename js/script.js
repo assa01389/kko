41fa44fb1efbc265b43eeadb6801e7827aae4559
assa01389 <assa01389@gmail.com>
2024-11-05 10:43:33 +0900

    수정중

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-$(document).ready(function() {
-    let $topMenu = $('header');
-    let menuOffset = $topMenu.offset().top;
+$(document).ready(function () {
+  let $topMenu = $("header");
+  let menuOffset = $topMenu.offset().top;
 
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > menuOffset) {
-            $topMenu.addClass('active');
-        } else {
-            $topMenu.removeClass('active');
-        }
-    });
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > menuOffset) {
+      $topMenu.addClass("active");
+    } else {
+      $topMenu.removeClass("active");
+    }
+  });
+
+  $("#card_slide").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  });
 });
