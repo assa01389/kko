71eed8df83c7b71729f6bce59731bd08daf1701b
assa01389 <assa01389@gmail.com>
2024-11-21 11:51:58 +0900

    제발요

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import ListNewWrap from "./ListNewWrap";
-import Banner from "./Banner";
-import ListCateWrap from "./ListCateWrap";
 import CardList from "./CardList";
+import ListNewWrap from "./ListNewWrap";
 
 function MainContent() {
   return (
+    <>
     <div className="main-content">
       <div className="left">
-        {/* 뉴스 */}
-        <ListNewWrap></ListNewWrap>
-        {/* 배너 */}
-        <Banner></Banner>
-        {/* 크루 */}
-        <ListCateWrap></ListCateWrap>
+      <ListNewWrap
       </div>
       <div className="right">
-        {/* 카드들 */}
         <CardList></CardList>
       </div>
     </div>
+    </>
   );
 }
 
